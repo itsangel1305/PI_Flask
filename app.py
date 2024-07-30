912735b9b0bce2c15dd2736acd8e626f4aa74a92
itsangel1305 <xitsangel11@gmail.com>
2024-07-29 19:16:20 -0600

    ghl

--- app.py
+++ app.py
@@ -1,47 +1,46 @@
-from flask import Flask,request,jsonify,render_template
+from flask import Flask, render_template, request
 
-app = Flask (__name__)
-
-#ruta simple
+app = Flask(__name__)
 
 @app.route('/')
-def principal():
-   return render_template('index.html')
+def index():
+    return render_template('index.html')
 
-@app.route('/inicioS')
-def inicioS():
-   return render_template('inicioS.html')
+@app.route('/inicio')
+def inicio():
+    return render_template('inicio.html')
 
-@app.route('/directorio')
-def directorio():
-   return render_template('directorioM.html')
-
-@app.route('/perfilMedico')
-def perfilMedico():
-   return render_template('perfilMed.html')
+@app.route('/registro')
+def registro():
+    return render_template('registro.html')
 
 @app.route('/registroMedico')
 def registroMedico():
-   return render_template('registroMed.html')
+    return render_template('registroMedico.html')
 
 @app.route('/registroPaciente')
 def registroPaciente():
-   return render_template('registro.html')
+    return render_template('registroPaciente.html')
+
+@app.route('/directorioM')
+def directorioM():
+    return render_template('directorioM.html')
+
+@app.route('/perfilMed')
+def perfilMed():
+    return render_template('perfilMed.html')
 
 @app.route('/registrOp')
 def registrOp():
-   return render_template('registrOp.html')
+    return render_template('registrOp.html')
 
 @app.route('/test')
 def test():
-   return render_template('test.html')
+    return render_template('test.html')
 
 @app.route('/respuestas')
 def respuestas():
-   return render_template('respuestas.html')
-
+    return render_template('respuestas.html')
 
 if __name__ == '__main__':
-  app.run(port=3000,debug=True)
-
-
+    app.run(debug=True)
